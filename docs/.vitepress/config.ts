e0866a75b7039ebca248f346fb41eedf535e522c
echo-sweet <echo@macarong.net>
2022-10-06 20:44:09 +0900

    fix github.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -54,15 +54,15 @@ const config = defineConfig({
             copyright: 'No Copy right'
         },
         socialLinks: [
-            { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
-            { icon: 'twitter', link: '...' },
-            // You can also add custom icons by passing SVG as string:
-            {
-              icon: {
-                svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>'
-              },
-              link: '...'
-            }
+            { icon: 'github', link: 'https://github.com/echo-youn/echo-log' },
+            // { icon: 'twitter', link: '...' },
+            // // You can also add custom icons by passing SVG as string:
+            // {
+            //   icon: {
+            //     svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>'
+            //   },
+            //   link: '...'
+            // }
         ],
         editLink: {
             pattern: 'https://github.com/echo-youn/echo-log/edit/main/docs/:path',
